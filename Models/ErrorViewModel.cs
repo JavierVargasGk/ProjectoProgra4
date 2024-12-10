86a11d8b9f1569339fef35a94ffea7490ab15ce7
Javier <javiermvm6@gmail.com>
2024-12-09 19:12:21 -0600

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectoProgra4.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
